c53b21a4fe0164fff47c65c32e83c1ff0427c4cc
KolesnikovAS <kalesnikau.aliaksandr@gmail.com>
2022-03-30 01:11:53 +0300

    added About section

---
+++ src/Components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Button.module.scss"
+
+const Button = (props) => {
+    return (
+        <button style={{backgroundColor: props.backgroundColor, color: props.color}} className={styles.button}>
+            {props.children}
+        </button>
+    )
+}
+
+export default Button;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,18 +2,46 @@ import React from "react";
 import styles from "./Home.module.scss"
 import slide1 from "../../Assets/images/Home/slide1.jpg"
 import arrow from "../../Assets/images/Home/arrow.png"
+import videoPoster from "../../Assets/images/Home/videoPoster.jpg"
+import Button from "../../Components/button/Button";
+import brendIcon1 from "../../Assets/images/Home/brendicon1.png";
+import brendIcon2 from "../../Assets/images/Home/brendicon2.png";
+import brendIcon3 from "../../Assets/images/Home/brendicon3.png";
+import brendIcon4 from "../../Assets/images/Home/brendicon4.png";
 
 const Home = () => {
     return (
-        <section className={styles.slider}>
-            <div className={styles.slider_content}>
-                <h1>Modern minimalist home</h1>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page.</p>
-                <button><img src={arrow} alt="arrow"/></button>
-                <button><img src={arrow} alt="arrow"/></button>
-            </div>
-            <img className={styles.slide_image} src={slide1} alt="design"/>
-        </section>
+        <main>
+            <section className={styles.slider}>
+                <div className={styles.slider_content}>
+                    <h1>Modern minimalist home</h1>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page.</p>
+                    <button><img src={arrow} alt="arrow" /></button>
+                    <button><img src={arrow} alt="arrow" /></button>
+                </div>
+                <img className={styles.slide_image} src={slide1} alt="design" />
+            </section>
+
+            <section className={styles.about}>
+                <div className={styles.promo}>
+                    <video src="#" width="883px" height="419px" controls="controls" poster={videoPoster} />
+                    <ul>
+                        <li><img src={brendIcon1} /><span>LOGOIPSUM</span></li>
+                        <li><img src={brendIcon2} /><span>logoipsum</span></li>
+                        <li><img src={brendIcon3} /><span>logoipsum</span></li>
+                        <li><img src={brendIcon4} /><span>LOGOIPSUM</span></li>
+                    </ul>
+                </div>
+                <div className={styles.discription}>
+                    <h2>About</h2>
+                    <p style={{ textAlign: "justify", marginBottom: "57px" }} >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.</p>
+                    <Button backgroundColor={"black"} color={"white"}>Learn More</Button>
+                    <p style={{ marginTop: "197px" }}>"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution."</p>
+                    <p style={{ color: "#BFBFBF", fontFamily: "Poppins-Bold, san-serif" }}>Ramones Aoky</p>
+                    <p style={{ color: "black", fontFamily: "Poppins-SemiBold, san-serif" }}>CEO Company</p>
+                </div>
+            </section>
+        </main>
     )
 }
 
