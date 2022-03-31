909ba51f637cd93c8d89b2db4ed2ace23c7b733a
KolesnikovAS <kalesnikau.aliaksandr@gmail.com>
2022-04-01 00:04:14 +0300

    added Services page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Containers/Header/Header"
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Footer from "./Containers/Footer/Footer";
+import Services from "./Pages/Services/Services";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/About" element={<About />} />
+                <Route path="/Services" element={<Services />} />
             </Routes>
             <Footer />
         </BrowserRouter>
---
+++ src/Pages/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageTitle from "../../Components/PageTitle/PageTitle";
+import styles from "./Services.module.scss"
+
+const Services = () => {
+    return (
+        <main>
+            <PageTitle title={"Services"} />
+        </main>
+    )
+}
+
+export default Services;
