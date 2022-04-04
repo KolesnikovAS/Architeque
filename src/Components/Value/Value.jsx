39d5224a6c868033517401e60c955597ea9cfad4
KolesnikovAS <kalesnikau.aliaksandr@gmail.com>
2022-04-04 23:11:29 +0300

    fixed Value component

--- src/Components/Value/Value.jsx
+++ src/Components/Value/Value.jsx
@@ -5,7 +5,7 @@ import styles from "./Value.module.scss"
 const Value = (props) => {
     return (
         <div className={styles.value}>
-            <img src={props.icon} title="icon" />
+            <img src={props.icon} alt="icon" />
             <h2>{props.number}</h2>
             <p>{props.title}</p>
         </div>
