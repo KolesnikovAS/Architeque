a9544bed847c428a36cfdda8bc7b62c2a04ade39
KolesnikovAS <kalesnikau.aliaksandr@gmail.com>
2022-04-08 21:22:18 +0300

    added burger menu for header

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -7,26 +7,28 @@ import phoneIcon from "../../Assets/images/Header/phone.png"
 
 const Header = () => {
     const links = [{ path: "/", value: "Home" },
-                    { path: "/About", value: "About" },
-                    { path: "/Services", value: "Services" },
-                    { path: "/Portfolio", value: "Portfolio" },
-                    { path: "/Teams", value: "Teams" }]
+    { path: "/About", value: "About" },
+    { path: "/Services", value: "Services" },
+    { path: "/Portfolio", value: "Portfolio" },
+    { path: "/Teams", value: "Teams" }]
     return (
         <header>
             <div className={styles.contacts_line}>
                 <img src={mailIcon} alt="mail" />
                 <span>mail@yourcompany.com</span>
                 <img src={phoneIcon} alt="phone" />
-                <span>+896 120 5889 (Toll free)</span>
+                <span><a href="tel:+8961205889">+896 120 5889 (Toll free)</a></span>
                 <span>Mon - Sat | 10am - 7pm</span>
             </div>
             <div className={styles.nav_line}>
                 <h2>Architeque</h2>
                 <nav>
-                    <ul>
+                    <input className={styles.menu_toggle} type="checkbox" id="menu_toggle" />
+                    <label className={styles.menu_button} for="menu_toggle"><span></span></label>
+                    <ul className={styles.menu_box}>
                         {links.map((link, index) => <li key={index}><NavLink
-                        className={({ isActive }) => `${isActive ? styles.active : ''}`} 
-                        to={link.path}>{link.value}</NavLink></li>)}
+                            className={({ isActive }) => `${isActive ? styles.active : ''}`}
+                            to={link.path}>{link.value}</NavLink></li>)}
                     </ul>
                 </nav>
                 <Button style={"black"} size={"medium"} title={"Let's start"} />
